8fac6764f31e2e43fc7a2391e70a372da6f7b77d
Vpandey-tech <contact@technewity.com>
2026-07-30 13:00:37 +0530

    Replace old pill logo with official Technewity Labs branding and fix JSX type lints

--- apps/frontend/components/Logo.tsx
+++ apps/frontend/components/Logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Logo() {
   return (
     <h2 className="logo w-14 h-14 inline-flex items-center justify-center">
-      <Image src={'/logo.png'} width={50} height={50} alt="Technewity Labs" />
+      <img src="/logo71x71.png" className="w-[50px] h-[50px] object-contain" alt="Technewity Labs" />
     </h2>
   )
 }
